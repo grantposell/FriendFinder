f149ba4558be110ba779ad04a9bd7d7cd8d933d1
grantposell <grantposell@gmail.com>
2018-04-25 23:34:20 -0500

    Added Github link

--- app/server.js
+++ app/server.js
@@ -10,3 +10,12 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'app/public')));
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function(){
+    console.log("App listening on port: " + PORT);
+})
