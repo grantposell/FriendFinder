a9c5f53583456d685fa84cc7800c7dcec9fbfee0
grantposell <grantposell@gmail.com>
2018-04-30 18:13:50 -0500

    Added more JS

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,12 @@
 var friends = require('../data/friends.js');
 
-// module.exports = function(app){
-//     app.get('/api/friends', function (req, res) {
-//         //This will be used to display a JSON of all possible friends.
-//         res.json(friends);
-//     })
+module.exports = function(app){
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    })
+    app.post('/api/friends', function(req, res) {
+        friends.push(req.body);
+        res.json(true);
+    });
+
+};
